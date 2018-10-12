d86c7a115bc396661120fded9cac8427198b752d
Chau Ho <sleightofhand044@gmail.com>
2018-10-12 16:20:05 -0700

    no message

--- HowToMakeAVideoGame/Assets/Scripts/PlayerCollision.cs
+++ HowToMakeAVideoGame/Assets/Scripts/PlayerCollision.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour {
 
     public PlayerMovement movement; // A reference to our PlayerMovement script
-   
+    public AudioSource QuackSource;
 
     // This function runs when we hit another object.
     // We get information about the collision and call it "collisionInfo".
+
+    void Start()
+    {
+        QuackSource = GetComponent<AudioSource>();
+    }
  
 
     void OnCollisionEnter (Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
+            QuackSource.Play();
+
             movement.enabled = false; // Disable the players movement.
             FindObjectOfType<GameManager>().EndGame();
           
